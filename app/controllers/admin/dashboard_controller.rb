fb0d69e5f15523bee4c1d3d93c51da6872f9f6fa
Peter Hashem <peterhashem@gmail.com>
2024-04-16 17:06:20 -0400

    show counts for products & categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,15 @@
 class Admin::DashboardController < ApplicationController
+  before_action :authenticate
+  
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
+  end
+
+  private
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
   end
 end
